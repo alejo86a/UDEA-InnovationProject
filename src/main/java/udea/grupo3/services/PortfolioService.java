44406e52918ae021148198c9800c3c1798545fc4
jcarangoGitHub <juancamiloarango@gmail.com>
2018-07-22 16:03:51 -0500

    corrección urls con problemas

--- src/main/java/udea/grupo3/services/PortfolioService.java
+++ src/main/java/udea/grupo3/services/PortfolioService.java
@@ -14,11 +14,11 @@ public class PortfolioService {
 
     public static Portfolio GET_PORTFOLIO_BY_ID(Integer idPortfolio) {
         Portfolio pf = null;
-        if (idPortfolio == 1 || idPortfolio == 2) {
+        if (idPortfolio >= 0 && idPortfolio < 5) {
             pf = new Portfolio();
             pf.setPortfolioId(idPortfolio);
-            pf.setName("my first portfolio");
-            pf.setDescription("my description");
+            pf.setName("my portfolio: " + idPortfolio);
+            pf.setDescription("my description: " + idPortfolio);
         }
 
         return pf;
@@ -27,18 +27,12 @@ public class PortfolioService {
     public List<Project> getProjects(Integer idPortfolio) {
         ArrayList<Project> result = new ArrayList<>();
         Project project = new Project();
-        if(idPortfolio == 1) {
-            project.setProjectId(1);
-            project.setIdPortFolio(1);
-            project.setName("VR Surgery");
-            project.setDescription("Achieve successful remote surgeries using VR technologies");
-        } else if(idPortfolio == 2) {
-            project.setProjectId(2);
-            project.idPortFolio(2);
-            project.setName("Knowledge Injection");
-            project.setDescription("Inject knowledge into the brain without going for year to the school");
-        }
+        project.setProjectId(1);
+        project.setIdPortFolio(idPortfolio);
+        project.setName("VR Surgery");
+        project.setDescription("Achieve successful remote surgeries using VR technologies");
         result.add(project);
+
         return result;
     }
     
